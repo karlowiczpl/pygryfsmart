b4e08e2fc8ff68c5cbe95240ba1fc4d4654587ee
karlowiczpl <karol.kar1@outlook.com>
2025-01-24 16:41:10 +0100

    initial commit

---
+++ tests/test_device.py
@@ -0,0 +1,71 @@
+import pytest
+import asyncio
+from unittest.mock import AsyncMock, patch
+
+from pygryfsmart.device import Device
+
+@pytest.fixture
+def mock_device():
+    device = Device(port="/dev/ttyUSB0")
+    device.send_data = AsyncMock()
+    device.open_connection = AsyncMock()
+    device.read_data = AsyncMock()
+    device.close_connection = AsyncMock()
+    return device
+
+@pytest.mark.asyncio
+async def test_set_out(mock_device):
+    await mock_device.set_out(1, 3, 1)
+    mock_device.send_data.assert_called_once_with("AT+SetOut=1,0,0,1,0,0,0\n\r")
+
+@pytest.mark.asyncio
+async def test_send_data(mock_device):
+    await mock_device.send_data("AT+TestCommand\n\r")
+    mock_device.send_data.assert_called_once_with("AT+TestCommand\n\r")
+
+@pytest.mark.asyncio
+async def test_parse_data_in(mock_device):
+    data = "I=1,1,0,1,0,0,1"
+    await mock_device._Device__parse_data(data)
+    assert mock_device.data["IN"] == {
+        1: {
+            1: 1,
+            2: 0,
+            3: 1,
+            4: 0,
+            5: 0,
+            6: 1,
+        }
+    }
+
+@pytest.mark.asyncio
+async def test_parse_data_out(mock_device):
+    data = "O=1,0,1,0,1,0,1"
+    await mock_device._Device__parse_data(data)
+    assert mock_device.data["OUT"] == {
+        1: {
+            1: 0,
+            2: 1,
+            3: 0,
+            4: 1,
+            5: 0,
+            6: 1,
+        }
+    }
+
+@pytest.mark.asyncio
+async def test_set_key_time(mock_device):
+    await mock_device.set_key_time(100, 200, 1, 2, 1)
+    mock_device.send_data.assert_called_once_with("AT+Key=1,2,100,200,1\n\r")
+
+@pytest.mark.asyncio
+async def test_set_rol(mock_device):
+    await mock_device.set_rol(1, 2, 100, 1)
+    mock_device.send_data.assert_called_once_with("AT+SetRol=1,100,0,1,0,0,102\n\r")
+
+@pytest.mark.asyncio
+async def test_reset(mock_device):
+    mock_device.data["OUT"] = {0: [1, 0, 1, 0, 1, 0]}
+    await mock_device.reset(0, update_states=True)
+    mock_device.send_data.assert_any_call("AT+RST=0\n\r")
+    mock_device.send_data.assert_any_call("AT+SetOut=0,1,0,1,0,1,0\n\r")
